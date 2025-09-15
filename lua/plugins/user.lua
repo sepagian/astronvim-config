bc1cf2d446b58d961202d89402608778cef3e50f
sepagian <rizkydana14@gmail.com>
2025-09-15 10:21:10 +0800

    added windsurf

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -76,4 +76,12 @@ return {
       )
     end,
   },
+  {
+    "Exafunction/windsurf.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "hrsh7th/nvim-cmp",
+    },
+    config = function() require("codeium").setup {} end,
+  },
 }
