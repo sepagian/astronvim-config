dbbf4798f8e21195d801032e4bf62618045c54b7
sepagian <rizkydana14@gmail.com>
2025-09-16 18:55:19 +0800

    added mini.nvim plugins

---
+++ lua/plugins/mini-nvim.lua
@@ -0,0 +1,45 @@
+return {
+  {
+    "nvim-mini/mini.bracketed",
+    version = "*",
+    config = function() require("mini.bracketed").setup() end,
+  },
+  {
+    "nvim-mini/mini.animate",
+    version = "*",
+    config = function() require("mini.animate").setup() end,
+  },
+  {
+    "nvim-mini/mini.icons",
+    opts = {
+      file = {
+        ["README.md"] = { glyph = "󰂺" },
+      },
+      filetype = {
+        c = { glyph = "" },
+        go = { glyph = "" },
+      },
+    },
+    specs = {
+      {
+        "nvim-neo-tree/neo-tree.nvim",
+        opts = {
+          default_component_configs = {
+            icon = {
+              provider = function(icon, node)
+                local text, hl
+                local mini_icons = require "mini.icons"
+                if node.type == "file" then
+                  text, hl = mini_icons.get("file", node.name)
+                end
+
+                if text then icon.text = text end
+                if hl then icon.highlight = hl end
+              end,
+            },
+          },
+        },
+      },
+    },
+  },
+}
