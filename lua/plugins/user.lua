4442a7fef59537920ed0824972b4f2cf1ff9ea89
sepagian <rizkydana14@gmail.com>
2025-11-12 12:07:20 +0800

    refactor(flash): remap flash.nvim key

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -67,15 +67,15 @@ return {
 
   {
     "folke/flash.nvim",
-    event = "VeryLazy",
-    ---@type Flash.Config
+    event = "verylazy",
+    ---@type flash.config
     opts = {},
     keys = {
-      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+      { "zk", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "flash" },
+      { "zK", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "flash treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "remote flash" },
+      { "r", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "treesitter search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "toggle flash search" },
     },
   },
 
